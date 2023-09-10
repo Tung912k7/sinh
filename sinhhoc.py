342806931cf95d6998b77b8cca54ffbdfe0bd31b
Thanh Tung <81634325+Tung912k7@users.noreply.github.com>
2023-09-10 23:40:26 +0700

    Update sinhhoc.py

--- sinhhoc.py
+++ sinhhoc.py
@@ -4,20 +4,37 @@ from kivy.core.window import Window
 from kivy.config import Config
 from kivy.utils import platform
 from kivy.uix.image import Image
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.widget import Widget
 
 
 Config.set('graphics', 'resizable', True)
-Builder.load_file('bg.kv')
-class MyLayout(Image):
-     pass
+
+class MainWindow(Screen):
+    pass
+
+class SecondWindow(Screen):
+    pass
+class ThirdWindow(Screen):
+    pass
+class FourthWindow(Screen):
+        pass
+class FifthWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file('main.kv')
+
 class MainApp(App):
     def build(self):
         if(platform == 'android'):
             Window.maximize()
         else:
-            Window.size = (480,800)
-        Window.clearcolor = (255,255,255,0)
-        return MyLayout()
+            Window.size = (480, 800)
+        Window.clearcolor = (255, 255, 255, 0)
+        return kv
 
         
 if __name__ == "__main__":
