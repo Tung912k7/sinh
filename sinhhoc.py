b6323aa3e248d7fe32cc25192a7730a7738e8851
Thanh Tung <81634325+Tung912k7@users.noreply.github.com>
2023-09-10 22:34:11 +0700

    Update sinhhoc.py

--- sinhhoc.py
+++ sinhhoc.py
@@ -1,20 +1,15 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.config import Config
 from kivy.utils import platform
-from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.image import Image
+
 
 Config.set('graphics', 'resizable', True)
-'''Builder.load_string(""" 
-<Image>
-		Image:
-			source: 'bg.png'
-			allow_stretch: False
-			keep_ratio: True
-			""")'''
+Builder.load_file('bg.kv')
+class MyLayout(Image):
+     pass
 class MainApp(App):
     def build(self):
         if(platform == 'android'):
@@ -22,7 +17,7 @@ class MainApp(App):
         else:
             Window.size = (480,800)
         Window.clearcolor = (255,255,255,0)
-        return
+        return MyLayout()
 
         
 if __name__ == "__main__":
